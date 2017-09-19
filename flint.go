81716bf0e331190061d78a39d5cf5bc3da082ec9
Kris Hunt <sourcekris@users.noreply.github.com>
2017-09-19 20:58:09 +1000

    Add a bunch more basic patterns like Cmp, String, BitLen, Int64 and Sign

--- flint.go
+++ flint.go
@@ -17,6 +17,7 @@ import "C"
 
 import (
   "runtime"
+  "unsafe"
 )
 
 type Fmpz struct {
@@ -25,7 +26,7 @@ type Fmpz struct {
 }
 
 // Finalizer - release the memory allocated to the fmpz
-func intFinalize(z *Fmpz) {
+func fmpzFinalize(z *Fmpz) {
 	if z.init {
 		runtime.SetFinalizer(z, nil)
 		C.fmpz_clear(&z.i[0])
@@ -39,7 +40,7 @@ func (z *Fmpz) doinit() {
 	}
 	z.init = true
 	C.fmpz_init(&z.i[0])
-	runtime.SetFinalizer(z, intFinalize)
+	runtime.SetFinalizer(z, fmpzFinalize)
 }
 
 // SetUint64 sets z to x and returns z.
@@ -61,4 +62,77 @@ func (z *Fmpz) SetInt64(x int64) *Fmpz {
 // NewFmpz allocates and returns a new Fmpz set to x.
 func NewFmpz(x int64) *Fmpz {
   return new(Fmpz).SetInt64(x)
+}
+
+// Cmp compares z and y and returns:
+//
+//   -1 if z <  y
+//    0 if z == y
+//   +1 if z >  y
+//
+func (z *Fmpz) Cmp(y *Fmpz) (r int) {
+  z.doinit()
+  y.doinit()
+  r = int(C.fmpz_cmp(&z.i[0], &y.i[0]))
+  if r < 0 {
+    r = -1
+  } else if r > 0 {
+    r = 1
+  }
+  return
+}
+
+// string returns z in the base given
+func (z *Fmpz) string(base int) string {
+  if z == nil {
+    return "<nil>"
+  }
+  z.doinit()
+  p := C.fmpz_get_str(nil, C.int(base), &z.i[0])
+  s := C.GoString(p)
+  C.free(unsafe.Pointer(p))
+  return s
+}
+
+// String returns the decimal representation of z.
+func (z *Fmpz) String() string {
+  return z.string(10)
+}
+
+// Int64 returns the int64 representation of z.
+// If z cannot be represented in an int64, the result is undefined.
+func (z *Fmpz) Int64() (y int64) {
+  if !z.init {
+    return
+  }
+  if C.fmpz_fits_si(&z.i[0]) != 0 {
+    return int64(C.fmpz_get_si(&z.i[0]))
+  }
+  // Undefined result if > 64 bits
+  if z.BitLen() > 64 {
+    return
+  }
+
+  return
+}
+
+// BitLen returns the length of the absolute value of z in bits.
+// The bit length of 0 is 0.
+func (z *Fmpz) BitLen() int {
+  z.doinit()
+  if z.Sign() == 0 {
+    return 0
+  }
+  return int(C.fmpz_sizeinbase(&z.i[0], 2))
+}
+
+// Sign returns:
+//
+//  -1 if x <  0
+//   0 if x == 0
+//  +1 if x >  0
+//
+func (z *Fmpz) Sign() int {
+  z.doinit()
+  return int(C.fmpz_sgn(&z.i[0]))
 }
--- flint_test.go
+++ flint_test.go
@@ -4,9 +4,72 @@ import (
   "testing"
 )
 
+func TestSign(t *testing.T) {
+  a := NewFmpz(1024)
+  b := NewFmpz(-1024)
+  c := NewFmpz(0)
+
+  if c.Sign() != 0 {
+    t.Error("Expected Sign of 0 to be 0.\n")
+  } 
+
+  if a.Sign() <= 0 {
+    t.Error("Expected Sign of 1024 to be positive but it wasnt.\n")
+  }
+
+  if b.Sign() >= 0 {
+    t.Error("Expected Sign of -1024 to be negative but it wasnt.\n")
+  }
+}
+
+func TestCmp(t *testing.T) {
+  a := NewFmpz(1024)
+  b := NewFmpz(1024)
+  c := NewFmpz(2048)
+
+  if a.Cmp(b) != 0 {
+    t.Error("Expected a.Cmp(b) == 0 but it wasnt.\n")
+  } 
+
+  if a.Cmp(c) > 0 {
+    t.Error("Expected a.Cmp(c) < 0 (i.e. a < c) but it wasnt.\n")
+  }
+
+  if c.Cmp(a) < 0 {
+    t.Error("Expected c.Cmp(a) > 0 (i.e. a > c) but it wasnt.\n")
+  }
+}
+
 func TestNewFmpz(t *testing.T) {
   a := NewFmpz(1)
-  if a != a {
-    t.Errorf("Expected a == a but got something else. Major bummer.\n")
+  if a.Cmp(a) != 0 {
+    t.Error("Expected a == a but got something else. Major bummer.\n")
+  } 
+}
+
+func TestString(t *testing.T) {
+  a := NewFmpz(1024)
+  aStr := a.String()
+
+  if aStr != "1024" {
+    t.Errorf("Expected aStr == 1024 but got: %s (something else?). Soz pal.\n", aStr)
+  } 
+}
+
+func TestInt64(t *testing.T) {
+  a := NewFmpz(64)
+  b := int64(64)
+  if a.Int64() != b {
+    t.Error("Expected a.Int64() == 64 but got something else.\n")
+  } 
+}
+
+func TestBitLen(t *testing.T) {
+  a := NewFmpz(65536)
+
+  b := a.BitLen()
+
+  if  b != 17 {
+    t.Errorf("Expected a.BitLen() == 17 but got something else: %d.\n", b)
   } 
 }
