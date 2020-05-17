6bb6e51940f1a64af7fa91408c22db1037e47eb0
kris <kris@sneakerz.org>
2020-05-17 17:51:00 +1000

    put FmpzMat type into its own file

--- flint.go
+++ flint.go
@@ -10,7 +10,6 @@ package goflint
 #cgo LDFLAGS: -lflint -lgmp
 #include <flint/flint.h>
 #include <flint/fmpz.h>
-#include <flint/fmpz_mat.h>
 #include <flint/fmpq.h>
 #include <flint/nmod_poly.h>
 #include <gmp.h>
@@ -25,19 +24,10 @@ fmpz *_fmpq_denref(fmpq_t op) {
     return fmpq_denref(op);
 }
 
-fmpz fmpzmat_get_val(fmpz_mat_t mat, slong pos) {
-	return mat->entries[pos];
-}
-
-void fmpzmat_set_val(fmpz_mat_t mat, fmpz_t val, slong pos) {
-	mat->entries[pos] = *val;
-}
-
 */
 import "C"
 
 import (
-	"errors"
 	"runtime"
 	"unsafe"
 )
@@ -86,14 +76,6 @@ type FlintRandT struct {
 	init bool
 }
 
-// FmpzMat is a matrix of Fmpz.
-type FmpzMat struct {
-	i    C.fmpz_mat_t
-	rows int
-	cols int
-	init bool
-}
-
 /*
  * Initializers and Finalizers
  */
@@ -104,17 +86,6 @@ func fmpzFinalize(z *Fmpz) {
 		runtime.SetFinalizer(z, nil)
 		C.fmpz_clear(&z.i[0])
 		z.init = false
-	}
-}
-
-// fmpzMatFinalize releases the memory allocated to the FmpzMat.
-func fmpzMatFinalize(m *FmpzMat) {
-	if m.init {
-		runtime.SetFinalizer(m, nil)
-		C.fmpz_mat_clear(&m.i[0])
-		m.init = false
-		m.rows = 0
-		m.cols = 0
 	}
 }
 
@@ -161,24 +132,6 @@ func (z *Fmpz) doinit() {
 	z.init = true
 	C.fmpz_init(&z.i[0])
 	runtime.SetFinalizer(z, fmpzFinalize)
-}
-
-// fmpzMatDoinit initializes an FmpzMat type.
-func (m *FmpzMat) fmpzMatDoinit(d ...int) error {
-	if m.init {
-		return nil
-	}
-	if len(d) == 2 {
-		m.rows = d[0]
-		m.cols = d[1]
-		m.init = true
-		C.fmpz_mat_init(&m.i[0], C.slong(m.rows), C.slong(m.cols))
-		runtime.SetFinalizer(m, fmpzMatFinalize)
-
-		return nil
-	}
-
-	return errors.New("fmpzMatDoinit: pass rows and colums on first init")
 }
 
 // fmpqDoinit initializes an Fmpz type.
@@ -251,13 +204,6 @@ func (z *Mpz) SetMpzInt64(x int64) *Mpz {
 // NewFmpz allocates and returns a new Fmpz set to x.
 func NewFmpz(x int64) *Fmpz {
 	return new(Fmpz).SetInt64(x)
-}
-
-// NewFmpzMat allocates a rows * cols matrix and returns a new FmpzMat.
-func NewFmpzMat(rows, cols int) *FmpzMat {
-	m := new(FmpzMat)
-	m.fmpzMatDoinit(rows, cols)
-	return m
 }
 
 // NewFmpq allocates and returns a new Fmpq set to p / q.
@@ -1212,79 +1158,6 @@ func (z *Fmpz) Randm(state *FlintRandT, m *Fmpz) *Fmpz {
 	C.fmpz_randm(&z.i[0], &state.i[0], &m.i[0])
 
 	return z
-}
-
-// Matrices.
-
-func (m *FmpzMat) String() string {
-	// Create a FILE * memstream.
-	var buf *C.char
-	var bufSize C.size_t
-	ms := C.open_memstream(&buf, &bufSize)
-	if ms == nil {
-		return ""
-	}
-	defer func() {
-		C.fclose(ms)
-		C.free(unsafe.Pointer(buf))
-	}()
-
-	if pp := C.fmpz_mat_fprint_pretty(ms, &m.i[0]); pp <= 0 {
-		// Positive value on success.
-		return ""
-	}
-
-	if rc := C.fflush(ms); rc != 0 {
-		// log.Warningf("fflush returned %d", rc)
-		return ""
-	}
-
-	return C.GoString(buf)
-}
-
-// Zero sets all values of matrix m to zero and returns m.
-func (m *FmpzMat) Zero() *FmpzMat {
-	C.fmpz_mat_zero(&m.i[0])
-	return m
-}
-
-// One sets diagonal values of matrix m to 1 and returns m.
-func (m *FmpzMat) One() *FmpzMat {
-	C.fmpz_mat_one(&m.i[0])
-	return m
-}
-
-// NumRows returns the number of rows in a FmpzMat matrix.
-func (m *FmpzMat) NumRows() int {
-	return int(C.fmpz_mat_nrows(&m.i[0]))
-}
-
-// NumCols returns the number of cols in a FmpzMat matrix.
-func (m *FmpzMat) NumCols() int {
-	return int(C.fmpz_mat_ncols(&m.i[0]))
-}
-
-// Entry returns the value at x, y in the matrix m.
-func (m *FmpzMat) Entry(x, y int) *Fmpz {
-	z := new(Fmpz)
-	z.doinit()
-	pos := x + m.cols*y
-	z.i[0] = C.fmpzmat_get_val(&m.i[0], C.slong(pos))
-	return z
-}
-
-// SetPosVal sets position pos in matrix m to val and returns m.
-func (m *FmpzMat) SetPosVal(val *Fmpz, pos int) *FmpzMat {
-	val.doinit()
-	C.fmpzmat_set_val(&m.i[0], &val.i[0], C.slong(pos))
-	return m
-}
-
-// SetVal sets position x, y in matrix m to val and returns m.
-func (m *FmpzMat) SetVal(val *Fmpz, x, y int) *FmpzMat {
-	val.doinit()
-	pos := x + m.cols*y
-	return m.SetPosVal(val, pos)
 }
 
 // Chinese Remainder Theorem.
